d010899ad6a8607947487f2cbbfe474e17fc3041
Pedro Escaleira <pedroescaleira@hotmail.com>
2025-07-28 23:42:43 +0100

    small fix
    Signed-off-by: Pedro Escaleira <pedroescaleira@hotmail.com>

--- src/main.py
+++ src/main.py
@@ -8,23 +8,19 @@ from transitions.extensions.nesting import HierarchicalMachine, NestedState
 NestedState.separator = "."
 
 
-def get_all_paths(machine: HierarchicalMachine):
-    paths = []
-
-    def dfs(state, path):
-        path.append(state)
-        transitions = [t for t in machine.get_transitions() if t.source == state]
-        if not transitions:  # If no outgoing transitions, it's a final state
-            paths.append(path[:])
-        else:
-            for transition in transitions:
-                dfs(transition.dest, path)
-        path.pop()
-
-    # Start DFS from the initial state
-    initial_state = machine.initial
-    dfs(initial_state, [])
-    return paths
+def get_most_inner_states(
+    machine: HierarchicalMachine, state: NestedState
+) -> List[NestedState]:
+    """
+    Get all inner states of a given state in a hierarchical machine.
+    """
+    inner_states = []
+    if state.states:
+        for substate in state.states.values():
+            inner_states.extend(get_most_inner_states(machine, substate))
+    else:
+        inner_states = [state]
+    return inner_states
 
 
 def get_paths_to_node(
@@ -44,7 +40,9 @@ def get_paths_to_node(
                 )  # Reverse the path to make it start from the initial state
         else:
             for transition in transitions:
-                if transition.source != transition.dest:        # otherwise, it is a foreach state transition
+                if (
+                    transition.source != transition.dest
+                ):  # otherwise, it is a foreach state transition
                     dfs(machine.get_state(transition.source), path)
         path.pop()
 
@@ -53,21 +51,47 @@ def get_paths_to_node(
     return paths
 
 
+def get_edge_node_info(state: NestedState):
+    edge_type = state.tags[0]
+
+    def deep_serialize(value):
+        if isinstance(value, dict):
+            return {k: deep_serialize(v) for k, v in value.items()}
+        elif isinstance(value, list):
+            return [deep_serialize(v) for v in value]
+        elif hasattr(value, "serialize") and callable(value.serialize):
+            if not hasattr(value, "_default_values"):
+                value._default_values = {}
+            return value.serialize().__dict__
+        else:
+            return value
+
+    edge_value = deep_serialize(state.metadata[list(state.metadata.keys())[0]])
+    if edge_type == "function":
+        if (
+            edge_value["type"] == "custom"
+            and (operation := edge_value["operation"].split(":"))[0] == "knative"
+        ):
+            edge_type = "function:knative"
+            edge_value = {"operation": operation[1].split("/")[1].split("?")[0]}
+        else:
+            edge_type = f"function:{edge_value['type']}"
+            edge_value = None  # TODO -> in the future, change this back to the original edge_value; for now, it is easier to debug without it
+
+    return {"type": edge_type, "value": edge_value}
+
+
 def get_nested_transition_path(
     machine: HierarchicalMachine,
     outer_state: NestedState,
     src_state: NestedState,
     machine_path,
 ):
-    # if not machine.get_nested_transitions(
-    #     src_path=[f"{machine_path}.{src_state.name}"]
-    # ):
-    #     return [[src_state]]
-    final_path = []
-    if machine.get_nested_transitions(
-        src_path=[f"{machine_path}.{src_state.name}"]
-    ):
-        path = get_nested_path(machine, src_state, path=f"{machine_path}.{src_state.name}")
+    final_path = {}
+    if machine.get_nested_transitions(src_path=[f"{machine_path}.{src_state.name}"]):
+        path = get_nested_path(
+            machine, src_state, path=f"{machine_path}.{src_state.name}"
+        )
         for t in machine.get_nested_transitions(
             src_path=[f"{machine_path}.{src_state.name}"]
         ):
@@ -83,8 +107,10 @@ def get_nested_transition_path(
             if path["type"] == "sequence":
                 for p in path["value"]:
                     transition_path.append(p.copy())
+                    # print(p)
                     if next_state_path["type"] == "sequence":
                         for nsp in next_state_path["value"]:
+                            # print(nsp)
                             transition_path.append(nsp)
                     else:
                         transition_path.append(next_state_path)
@@ -95,23 +121,33 @@ def get_nested_transition_path(
                         transition_path.append(nsp)
                 else:
                     transition_path.append(next_state_path)
+            # print(transition_path)
             final_path = {"type": "sequence", "value": transition_path}
     elif src_state.states:
-        path = get_nested_path(machine, src_state, path=f"{machine_path}.{src_state.name}")
+        path = get_nested_path(
+            machine, src_state, path=f"{machine_path}.{src_state.name}"
+        )
         final_path = path
     else:
-        final_path = {"type": "sequence", "value": [{"type": src_state.tags[0], "value": src_state}]}
+        # if src_state.metadata and (f := src_state.metadata.get("function")):
+        #     print(f)
+        final_path = {
+            "type": "sequence",
+            "value": [get_edge_node_info(state=src_state)],
+        }
     return final_path
 
 
-def get_nested_path(
-    machine: HierarchicalMachine, state: NestedState, path: str
-) -> List[List[Tuple[NestedState]]]:
+def get_nested_path(machine: HierarchicalMachine, state: NestedState, path: str):
     if not state.states:
-        return {"type": "sequence", "value": [{"type": state.tags[0], "value": state}]}
+        # if state.metadata and (f := state.metadata.get("function")):
+        #     print(f)
+        return {"type": "sequence", "value": [get_edge_node_info(state=state)]}
 
     if type(state.initial) == str:
-        path = get_nested_transition_path(machine, state, state.states[state.initial], path)
+        path = get_nested_transition_path(
+            machine, state, state.states[state.initial], path
+        )
     else:
         parallel = []
         for s in state.initial:
@@ -122,29 +158,29 @@ def get_nested_path(
     return path
 
 
-def get_paths_to_substate(machine: HierarchicalMachine, target_substate: str):
+def get_paths_to_substate(machine: HierarchicalMachine, target_substate: NestedState):
     def find_outer_path_to_substate(
         machine: HierarchicalMachine,
         outer_state: NestedState,
         state: NestedState,
-        target_substate: str,
+        target_substate: NestedState,
     ):
         outer_paths = []
         if substates := state.states:
-            for name, substate in substates.items():
+            for substate in substates.values():
                 outer_paths.extend(
                     find_outer_path_to_substate(
                         machine, outer_state, substate, target_substate
                     )
                 )
-                if name == target_substate:
+                if substate == target_substate:
                     outer_paths.extend(get_paths_to_node(machine, outer_state))
         return outer_paths
 
     # First, let's find the machine state where the target substate is
     outer_paths = []
     for state in machine.states.values():
-        if state.name != target_substate:
+        if state != target_substate:
             outer_paths.extend(
                 find_outer_path_to_substate(machine, state, state, target_substate)
             )
@@ -157,8 +193,11 @@ def get_paths_to_substate(machine: HierarchicalMachine, target_substate: str):
         for node in path[:-1]:
             np = get_nested_path(machine, node, node.name)
             if np["type"] == "sequence":
-                new_path["value"].append(np["value"] if len(np["value"]) > 1 else np["value"][0])
-            else:       # TODO should verify if it is of type parallel
+                new_path["value"].extend(np["value"])
+                # new_path["value"].append(
+                #     np["value"] if len(np["value"]) > 1 else np["value"][0]
+                # )
+            else:  # TODO should verify if it is of type parallel
                 new_path["value"].append(np)
         # For the last node in the path
         last_node_name = path[-1].name
@@ -206,48 +245,49 @@ def get_paths_to_substate(machine: HierarchicalMachine, target_substate: str):
     return paths
 
 
+def print_path(path, start=""):
+    if path.get("type") == "sequence":
+        print(f"{start}Sequence")
+        for v in path.get("value"):
+            print_path(v, start=f"\t{start}")
+    elif path.get("type") == "parallel":
+        print(f"{start}Parallel")
+        for v in path.get("value"):
+            print_path(v, start=f"\t{start}")
+    else:
+        print(f"{start}{path.get('type')}: {path.get('value')}")
+
+
 def main():
     subflows = []
-    with open("../test-workflows/valve.sw.yaml") as f:
+    # with open("../test-workflows/valve.sw.yaml") as f:
+    #     workflow = Workflow.from_source(f.read())
+    # with open("../test-workflows/valve-advertise-listing.sw.yaml") as f:
+    #     subflows.append(Workflow.from_source(f.read()))
+    # with open("../test-workflows/test.sw.yaml") as f:
+    #     subflows.append(Workflow.from_source(f.read()))
+    with open("../test-workflows/bank-app.yaml") as f:
         workflow = Workflow.from_source(f.read())
-    with open("../test-workflows/valve-advertise-listing.sw.yaml") as f:
-        subflows.append(Workflow.from_source(f.read()))
-    with open("../test-workflows/test.sw.yaml") as f:
-        subflows.append(Workflow.from_source(f.read()))
 
     machine = CustomHierarchicalMachine(
         model=None,
         initial=None,
         auto_transitions=False,
-    )    
+    )
     StateMachineGenerator(
-            workflow=workflow,
-            state_machine=machine,
-            get_actions=True,
-            subflows=subflows,
-        ).generate()
+        workflow=workflow,
+        state_machine=machine,
+        get_actions=True,
+        subflows=subflows,
+    ).generate()
 
-    # target_node = "d2"
-    # paths = get_paths_to_node(machine, machine.get_state(target_node))
-    # # for path in paths:
-    # #     print(" -> ".join([s.name for s in path]))
-
-    target_substate = "test"  # Replace with the actual substate name
-    paths_to_substate = get_paths_to_substate(machine, target_substate)
-    def print_path(path, start=""):
-        if path.get('type') == 'sequence':
-            print(f"{start}Sequence")
-            for v in path.get('value'):
-                print_path(v, start=f"\t{start}")
-        elif path.get('type') == 'parallel':
-            print(f"{start}Parallel")
-            for v in path.get('value'):
-                print_path(v, start=f"\t{start}")
-        else:
-            print(f"{start}{path.get('type')}: {path.get('value')}")
-        
-    for path in paths_to_substate:
-        print_path(path)
+    for state in machine.states.values():
+        for substate in get_most_inner_states(machine, state):
+            if substate.metadata and "function" in substate.metadata:
+                paths_to_substate = get_paths_to_substate(machine, substate)
+                print(substate.name, paths_to_substate, sep=" -> ")
+                # for path in paths_to_substate:
+                #     print_path(path)
 
 
 if __name__ == "__main__":
